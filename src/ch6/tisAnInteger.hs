79d3f01e766f689c5fd7b86bf329d68e3136feca
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-06-01 08:44:39 -0400

    Add some exercises for chapter 6.

---
+++ src/ch6/tisAnInteger.hs
@@ -0,0 +1,7 @@
+-- tisAnInteger.hs
+
+data TisAnInteger =
+  TisAn Integer
+
+instance Eq TisAnInteger where
+  (==) (TisAn a) (TisAn b) = a == b
---
+++ src/ch6/tuple.hs
@@ -0,0 +1,7 @@
+-- tuple.hs
+
+data Tuple a b =
+  Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple a1 a2) (Tuple b1 b2) = a1 == b1 && a2 == b2
